463230c313a2d4e5df49f856bd777b5d7dcaa8d2
shrihari <shrihari.p4@gmail.com>
2024-01-30 06:57:28 +0100

    mvp timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -189,7 +189,7 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-						MVP in 5 days. Gauranteed.
+						MVP in 10 days. Gauranteed.
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
 						We are a tech studio working at the intersection of design
