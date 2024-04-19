701bf92d3561c58c73ed9268ef120a4456f44255
Shrihari <shrihari.p4@gmail.com>
2024-04-19 20:45:08 +0200

    Update RootLayout.tsx

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -146,10 +146,12 @@ function Navigation() {
         <NavigationItem href="/work">Our Work</NavigationItem>
         <NavigationItem href="/about">About Us</NavigationItem>
       </NavigationRow>
+      { /*
       <NavigationRow>
         <NavigationItem href="/process">Our Process</NavigationItem>
         <NavigationItem href="/blog">Blog</NavigationItem>
       </NavigationRow>
+      */}
     </nav>
   )
 }
